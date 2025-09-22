00f207c7e3dff9df1201e736278b591544d46747
Rahul Narayanan <rahulon@icloud.com>
2025-09-22 19:49:08 -0400

    initial commit

---
+++ JotDown/JotDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotDownApp.swift
+//  JotDown
+//
+//  Created by Rahul on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct JotDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
