8363620744915dc3e88c7478c39bef8c4ba196b6
dahyunh <dahyunhong0@gmail.com>
2025-09-25 19:03:46 -0400

    Update ProfileView UI

--- JotDown/Views/ProfileView.swift
+++ JotDown/Views/ProfileView.swift
@@ -18,30 +18,29 @@ struct ProfileView: View {
     
     var body: some View {
         NavigationStack {
-            Form {
+            VStack{
                 if let user = user {
-                    Section("Name") {
-                        TextField("Enter your name", text: Binding(
-                            get: { user.name },
-                            set: { user.name = $0 }
-                        ))
-                    }
-                    Section("Bio") {
-                        // FIXME: Implement
-                        TextField("Describe yourself...", text: Binding(
-                            get: { user.bio },
-                            set: { user.bio = $0 }
-                        ))
-                    }
-                    Section("Categories") {
-                        // FIXME: Implement
-                        ForEach(categories, id: \.name) { category in
-                            Text(category.name)
+                    Text(user.name)
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                    Form {
+                        Section("Bio") {
+                            // FIXME: Implement
+                            TextField("Describe yourself...", text: Binding(
+                                get: { user.bio },
+                                set: { user.bio = $0 }
+                            ))
+                        }
+                        Section("Categories") {
+                            // FIXME: Implement
+                            ForEach(categories, id: \.name) { category in
+                                Text(category.name)
+                            }
                         }
                     }
                 } else {
-                    Text("No User Found.")
-                        .foregroundColor(.secondary)
+                    Text("No user found")
                 }
             }
             .navigationTitle("Profile")
@@ -55,5 +54,12 @@ struct ProfileView: View {
 }
 
 #Preview {
-    ProfileView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: User.self, configurations: config)
+    let context = container.mainContext
+    let previewUser = User(name: "Preview User", bio: "Loves iOS dev")
+    context.insert(previewUser)
+
+    return ProfileView()
+        .modelContainer(container)
 }
