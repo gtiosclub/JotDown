eec6637509530f2daea184f54d682cf13b846927
Drew Mendelow <drew.mendelow@gmail.com>
2025-09-24 21:54:50 -0400

    display dateCreated in ThoughtListView

--- JotDown/Views/ThoughtsListView.swift
+++ JotDown/Views/ThoughtsListView.swift
@@ -11,10 +11,29 @@ import SwiftUI
 struct ThoughtsListView: View {
     @Query(sort: \Thought.dateCreated, order: .reverse) var thoughts: [Thought]
     
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "M/dd/yyyy"
+        return formatter
+    }()
+    
+    private static let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        return formatter
+    }()
+    
     var body: some View {
         List {
             ForEach(thoughts) { thought in
-                Text(thought.content)
+                HStack {
+                    Text(thought.content)
+                    Spacer()
+                    VStack {
+                        Text(ThoughtsListView.dateFormatter.string(from: thought.dateCreated)).font(.caption)
+                        Text(ThoughtsListView.timeFormatter.string(from: thought.dateCreated)).font(.caption)
+                    }
+                }
             }
         }
         .navigationTitle("Thoughts")
