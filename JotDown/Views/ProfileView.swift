9c0d794ecd379656a6bdad545896af1bb8ff05a4
Stepan Kravtsov <Stepankravtsov0402@gmail.com>
2025-09-25 19:16:52 -0400

    active inactive categories ui

--- JotDown/Views/ProfileView.swift
+++ JotDown/Views/ProfileView.swift
@@ -12,19 +12,47 @@ struct ProfileView: View {
     @Environment(\.dismiss) private var dismiss
     @Query var users: [User]
     private var user: User? { users.first }
+//    @Query var categories: [Category]
+    @State private var bio: String = ""
+    @State private var showArchivedCategories: Bool = false
+    private var activeCategories: [Category] {
+        Category.dummyCategories.filter{$0.isActive}
+    }
+    private var inactiveCategories: [Category] {
+        Category.dummyCategories.filter{!$0.isActive}
+    }
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("Bio") {
                     // FIXME: Implement
-                    TextField("Describe yourself...", text: .constant(""))
+                    TextField("Desribe yourself...", text: $bio)
                 }
                 
-                Section("Categories") {
-                    // FIXME: Implement
+                Section("Active Categories") {
+                    ForEach(activeCategories) { category in
+                        Text(category.name)
+                            .swipeActions(allowsFullSwipe: true) {
+                                Button(role: .destructive, action: {
+                                    withAnimation {
+                                        category.isActive.toggle()
+                                    }
+                                }) {
+                                   Text("Archive")
+                                }
+                            }
+                    }
+                    NavigationLink(destination: ArchivedCategoriesView(cateogries: inactiveCategories)) {
+                        Text("\(inactiveCategories.count) inactive \(String(inactiveCategories.count).last == "1" ? "category" : "categories")")
+                    }
+                    .disabled(inactiveCategories.count == 0)
                 }
+                
+                
+                
             }
+            
             .navigationTitle("Profile")
             .toolbar {
                 Button(role: .close) {
