a462b6a8109a688d66d567fc4ef7060f472e8fe4
nahua3730 <nahua3730@gmail.com>
2025-09-25 18:04:06 -0400

    Add ProfileView with name, bio, and categories sections

--- JotDown/Views/ProfileView.swift
+++ JotDown/Views/ProfileView.swift
@@ -10,19 +10,38 @@ import SwiftData
 
 struct ProfileView: View {
     @Environment(\.dismiss) private var dismiss
-    @Query var users: [User]
+    
+    @Query private var users: [User]
+    @Query private var categories: [Category]
     private var user: User? { users.first }
+
     
     var body: some View {
         NavigationStack {
             Form {
-                Section("Bio") {
-                    // FIXME: Implement
-                    TextField("Describe yourself...", text: .constant(""))
-                }
-                
-                Section("Categories") {
-                    // FIXME: Implement
+                if let user = user {
+                    Section("Name") {
+                        TextField("Enter your name", text: Binding(
+                            get: { user.name },
+                            set: { user.name = $0 }
+                        ))
+                    }
+                    Section("Bio") {
+                        // FIXME: Implement
+                        TextField("Describe yourself...", text: Binding(
+                            get: { user.bio },
+                            set: { user.bio = $0 }
+                        ))
+                    }
+                    Section("Categories") {
+                        // FIXME: Implement
+                        ForEach(categories, id: \.name) { category in
+                            Text(category.name)
+                        }
+                    }
+                } else {
+                    Text("No User Found.")
+                        .foregroundColor(.secondary)
                 }
             }
             .navigationTitle("Profile")
