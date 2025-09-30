44e283ee7932de535f84564a727b3ddb87bf8d14
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2025-09-30 12:36:18 -0400

    implement categorizer function (#27)

--- JotDown/Intelligence/Categorizer.swift
+++ JotDown/Intelligence/Categorizer.swift
@@ -6,9 +6,25 @@
 //
 
 import FoundationModels
+import Foundation
+import SwiftUI
+import SwiftData
 
 class Categorizer {
+    let session = FoundationModels.LanguageModelSession()
+    
     func categorizeThought(_ thought: Thought, categories: [Category]) async throws {
-        // FIXME: Implement
+        let prompt = "Using this array of categories: [\(categories.enumerated().map { $0.element.name }.joined(separator: ", "))], which category do you think the thought '\(thought.content)' fits best in? Return only the name of the best category of fit and nothing else."
+        
+        let categoryName = try await session.respond(to: prompt)
+        var categoryIndex: Int?
+        for (index, category) in categories.enumerated() where category.name.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) == categoryName.content.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) {
+            categoryIndex = index
+        }
+        guard let index = categoryIndex else {
+            return
+        }
+        
+        thought.category = categories[index]
     }
 }
