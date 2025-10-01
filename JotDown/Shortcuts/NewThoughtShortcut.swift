5d9b89264addb921a5a079acbc05f4650979c2b3
cnowdev <siddhu.vsss@gmail.com>
2025-10-01 00:06:40 -0400

    added app dependency for the model container and parameters for title and content

--- JotDown/Shortcuts/NewThoughtShortcut.swift
+++ JotDown/Shortcuts/NewThoughtShortcut.swift
@@ -6,14 +6,28 @@
 //
 
 import AppIntents
+import SwiftData
 
 struct NewThoughtShortcut: AppIntent {
+    @AppDependency var modelContainer: ModelContainer
+    
+    @Parameter(title: "ThoughtTitle")
+    var thoughtTitle: String
+
+
+    @Parameter(title: "ThoughtContent")
+    var thoughtContent: String
+    
     static var title: LocalizedStringResource = "New Thought"
     static var description: IntentDescription? = IntentDescription("Creates a new thought in JotDown")
 
     @MainActor
     func perform() async throws -> some IntentResult {
         // TODO: Prompts the user and creates a new Thought
+        
+        
+        
+        
         return .result()
     }
 }
