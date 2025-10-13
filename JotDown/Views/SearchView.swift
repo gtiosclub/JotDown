02f40b83b134362f6c344c9cdcae41fdd0486c01
Drew Mendelow <drew.mendelow@gmail.com>
2025-10-13 10:25:13 -0400

    move foundation models search to FoundationModelSearchService

--- JotDown/Views/SearchView.swift
+++ JotDown/Views/SearchView.swift
@@ -99,7 +99,7 @@ struct SearchView: View {
         case .foundationModels:
             isSearching = true
             Task {
-                let r = await searchFoundationModels(query: query, in: thoughts)
+                let r = await FoundationModelSearchService.getRelevantThoughts(query: query, in: thoughts)
                 await MainActor.run {
                     results = r
                     isSearching = false
@@ -121,62 +121,6 @@ struct SearchView: View {
 
     private func searchRegexContains(query: String, in thoughts: [Thought]) -> [Thought] {
         thoughts.first.map { [$0] } ?? []
-    }
-
-    private func searchFoundationModels(query: String, in thoughts: [Thought]) async -> [Thought] {
-        let session = FoundationModels.LanguageModelSession()
-        
-        //1. First try to identify the category of the query
-        var uniqueCategories: [String] = []
-        
-        for thought in thoughts {
-            let name = thought.category.name
-            if (!uniqueCategories.contains(name)) {
-                uniqueCategories.append(name)
-            }
-        }
-        
-        
-        //Makes sure array is not empty
-        if uniqueCategories.isEmpty {
-            return []
-        }
-        
-        do {
-            //2. Determine the cateogry of the query
-            let categoryPrompt = """
-            Given the search query: "\(query)"
-            And these available categories: [\(uniqueCategories.joined(separator: ", "))]
-            
-            Which category is most likely to contain thoughts related to this search query?
-            Also, make a list of keywords from the query that can be used to find the most relevant thoguhts.
-            Return only the exact category name from the list of categories above, a list of keywords from the query, and nothing else.
-            """
-            
-            let queryResponse: QueryResponse = try await session.respond(to: categoryPrompt, generating: QueryResponse.self).content
-            
-            let selectedCategory = queryResponse.category.trimmingCharacters(in: .whitespacesAndNewlines)
-            
-            let categoryFilteredThoughts = thoughts.filter { $0.category.name.lowercased() == selectedCategory.lowercased() }
-            
-            
-            //3. Filter thoughts based on queries
-            var relevantThoughts: [Thought] = []
-            
-            for thought in categoryFilteredThoughts {
-                for keyword in queryResponse.keywords {
-                    if thought.content.lowercased().contains(keyword.lowercased()) {
-                        relevantThoughts.append(thought)
-                        break
-                    }
-                }
-            }
-            return relevantThoughts
-            
-        } catch {
-            print("Error in Foundation Models search: \(error)")
-            return []
-        }
     }
     
 
