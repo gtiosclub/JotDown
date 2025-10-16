41f8eb203a35a80a7a4b72baeb261567da5a71d0
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2025-10-16 15:28:45 -0400

    60 implement home page design (#69)
    * format homeview
    
    * format of homeview pages
    
    * create horizontal card list for thoughts
    
    * add card shadows and have text var be on homeview
    
    * Implement a custom tab bar with proper images and update HomeView to include HeaderHomeView and FooterHomeView, applying the new gradient background.
    
    * snap cards and connect footer as well as plus button
    
    * edit plus button and add checkmark button
    
    * fix small changes
    
    * ignore safe area with keyboard, unfocus keyboard on clicking somewhere else, and add keyboard done button
    
    ---------
    
    Co-authored-by: Ress1206-bit <adam.ress@icloud.com>

---
+++ JotDown/Views/HomeView/FooterHomeView.swift
@@ -0,0 +1,53 @@
+//
+//  FooterHomeView.swift
+//  JotDown
+//
+//  Created by Drew Mendelow on 10/14/25.
+//
+
+import SwiftUI
+
+struct FooterHomeView: View {
+    var noteCount: Int
+    var date: Date
+    
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE, MMM d"
+        return formatter
+    }()
+    
+    var body: some View {
+        HStack(alignment: .bottom) {
+          
+            VStack(spacing: 0) {
+                Text("\(noteCount)")
+                  .font(Font.custom("SF Pro", size: 36))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+                Text("notes")
+                  .font(Font.custom("SF Pro", size: 15))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+            }
+          
+            Spacer()
+            
+            VStack {
+                Text(Calendar.current.isDateInToday(date) ? "Today" : FooterHomeView.dateFormatter.string(from: date))
+                  .font(Font.custom("SF Pro", size: 36))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+                
+                Text("date")
+                  .font(Font.custom("SF Pro", size: 15))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+            }
+          
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 0)
+        .frame(alignment: .bottom)
+    }
+}
