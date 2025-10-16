2f0d35bb32bd0b50157570fb5510ef331c09a7fb
NeelMaddu268 <neilarm007@gmail.com>
2025-10-16 00:35:01 -0400

    Reversed my temporary changes to the Thought model class. Copied the DasboardView, ContentView, and NoteCategoryView code from the current PR. I need the code to ensure that clicking on the specific category works properly.

--- JotDown/Model/Thought.swift
+++ JotDown/Model/Thought.swift
@@ -15,7 +15,7 @@ class Thought {
     var category: Category
     var vectorEmbedding: [Double]
         
-    init(content: String, category: Category? = nil) {
+    init(content: String) {
         self.dateCreated = Date()
         self.content = content
         self.category = Category(name: "Dummy")
