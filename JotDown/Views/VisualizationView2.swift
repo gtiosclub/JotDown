e02ab828ebfb001d3ed9cf0058be585691f37094
cnowdev <siddhu.vsss@gmail.com>
2025-10-23 19:03:49 -0400

    added new experimental view - added RadialLayout layout and /Layouts folder

---
+++ JotDown/Views/VisualizationView2.swift
@@ -0,0 +1,36 @@
+//
+//  VisualizationView2.swift
+//  JotDown
+//
+//  Created by Siddharth Palanivel on 10/23/25.
+//
+
+import SwiftUI
+
+struct VisualizationView2: View {
+    var body: some View {
+        ScrollView([.horizontal, .vertical]) {
+            RadialLayout {
+                Text("test")
+                Text("test")
+                Text("test")
+                Text("test")
+                Text("test")
+                Text("test")
+                Text("test")
+                Text("test")
+            } .frame(width: 1000, height: 1000)
+        }
+        .defaultScrollAnchor(.center)
+
+        
+        
+    }
+}
+
+
+
+
+#Preview {
+    VisualizationView2()
+}
