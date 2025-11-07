c9e56e216b51c1ae9b6c61a1cf3939ed2bf1ea5e
Jeet Ajmani <67179741+jeetajmani@users.noreply.github.com>
2025-11-07 16:25:22 -0500

    updated select + edit functions, added delete function

--- JotDown/Views/HomeView/HeaderHomeView.swift
+++ JotDown/Views/HomeView/HeaderHomeView.swift
@@ -15,13 +15,67 @@ struct HeaderHomeView: View {
     @FocusState var isFocused: Bool
     let addThought: () async throws -> Void
     let saveEditedThought: () async throws -> Void
+    let deleteSelectedThoughts: () async throws -> Void
     @Binding var isSelecting: Bool
+    @Binding var isEditing: Bool
     @Binding var selectedThoughts: Set<Thought>
     @Binding var thoughtBeingEdited: Thought?
     
     var body: some View {
         HStack {
-            if isSelecting {
+            if isEditing {
+                Button {
+                    isSelecting = false
+                    selectedThoughts.removeAll()
+                    isEditing = false
+                    isFocused = true
+                    selectedIndex = 0
+                    thoughtInput = ""
+                } label: {
+                    HStack(spacing: 4) {
+                        Image(systemName: "chevron.left")
+                        Text("cancel")
+                    }
+                }
+                .font(Font.custom("SF Pro", size: 20))
+                .foregroundColor(Constants.TextLightText)
+                .padding(0)
+                
+                Spacer()
+                
+                Button {
+                    selectedIndex = 0
+                    isFocused = true
+                    isEditing = false
+                    Task {
+                        try await saveEditedThought()
+                    }
+                } label: {
+                    Text("done")
+                        .font(Font.custom("SF Pro", size: 15)
+                            .weight(.medium))
+                        .foregroundColor(.white)
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.white)
+                        .font(.system(size: 15, weight: .medium))
+                        .frame(width: 15, height: 15)
+                }
+                .disabled(selectedThoughts.count != 1)
+                .opacity(selectedThoughts.count != 1 ? 0.6: 1.0)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 10)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.61, green: 0.63, blue: 1), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.43, green: 0.44, blue: 0.81), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    )
+                )
+                .cornerRadius(25)
+            } else if isSelecting {
                 // MARK: - Selection Mode UI
                 Button {
                     isSelecting = false
@@ -32,34 +86,76 @@ struct HeaderHomeView: View {
                         Text("cancel")
                     }
                 }
-                .font(.system(size: 15, weight: .regular))
-                .foregroundColor(Color.blue)
+                .font(Font.custom("SF Pro", size: 20))
+                .foregroundColor(Constants.TextLightText)
+                .padding(0)
                 
                 Spacer()
                 
-                Button("edit") {
+                Button {
                     if let thoughtToEdit = selectedThoughts.first {
                         thoughtInput = thoughtToEdit.content
                         thoughtBeingEdited = thoughtToEdit
                         selectedIndex = 0
                         isSelecting = false
                         isFocused = true
+                        isEditing = true
                     }
+                } label: {
+                    Text("edit")
+                        .font(Font.custom("SF Pro", size: 15)
+                            .weight(.medium))
+                        .foregroundColor(.white)
+                    Image(systemName: "pencil")
+                        .foregroundColor(.white)
+                        .font(.system(size: 15, weight: .medium))
+                        .frame(width: 15, height: 15)
                 }
-                .font(.system(size: 15, weight: .medium))
-                .foregroundColor(.gray.opacity(selectedThoughts.count == 1 ? 1 : 0.4))
                 .disabled(selectedThoughts.count != 1)
-                .buttonStyle(.glass)
-                .buttonBorderShape(.capsule)
+                .opacity(selectedThoughts.count != 1 ? 0.6: 1.0)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 10)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.61, green: 0.63, blue: 1), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.43, green: 0.44, blue: 0.81), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    )
+                )
+                .cornerRadius(25)
                 
-                Button("delete") {
-                    // TODO
+                Button {
+                    Task {
+                        try await deleteSelectedThoughts()
+                    }
+                } label: {
+                    Text("delete")
+                        .font(Font.custom("SF Pro", size: 15)
+                            .weight(.medium))
+                        .foregroundColor(.white)
+                    Image(systemName: "trash")
+                        .foregroundColor(.white)
+                        .font(.system(size: 15, weight: .medium))
+                        .frame(width: 15, height: 15)
                 }
-                .font(.system(size: 15, weight: .medium))
-                .foregroundColor(selectedThoughts.isEmpty ? .gray.opacity(0.4) : .blue)
                 .disabled(selectedThoughts.isEmpty)
-                .buttonStyle(.glass)
-                .buttonBorderShape(.capsule)
+                .opacity(selectedThoughts.isEmpty ? 0.6: 1.0)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 10)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.61, green: 0.63, blue: 1), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.43, green: 0.44, blue: 0.81), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    )
+                )
+                .cornerRadius(25)
                 
             } else {
                 // MARK: - Normal Mode UI
@@ -67,38 +163,60 @@ struct HeaderHomeView: View {
                 
                 Spacer()
                 
-                Button("select") {
+                Button {
                     isSelecting = true
                     isFocused = false
+                } label: {
+                    Text("select")
+                        .font(Font.custom("SF Pro", size: 15)
+                            .weight(.medium))
+                        .foregroundColor(.white)
                 }
-                .font(.system(size: 15, weight: .regular))
-                .foregroundColor(Color.blue)
-                .padding(.horizontal, 8)
-                .padding(.vertical, 6)
-                .buttonStyle(.glass)
-                .buttonBorderShape(.capsule)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 10)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.61, green: 0.63, blue: 1), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.43, green: 0.44, blue: 0.81), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    )
+                )
+                .disabled(selectedIndex == 0)
+                .opacity(selectedIndex == 0 ? 0.6 : 1.0)
+                .cornerRadius(25)
                 
                 Button {
                     isFocused = false
-
-                    if thoughtBeingEdited != nil {
-                        Task {
-                            try await saveEditedThought()
-                        }
-                    } else {
-                        Task {
-                            try await addThought()
-                        }
+        
+                    Task {
+                        try await addThought()
                     }
-
+ 
                 } label: {
                     Image(systemName: selectedIndex != 0 ? "plus" : "checkmark")
+                        .fontWeight(.light)
                         .font(.system(size: 30))
+                        .foregroundStyle(Color(red: 109/255, green: 134/255, blue: 166/255))
+                        .padding(.vertical, 10)
+                        .frame(width: 30, height: 30)
                 }
             }
         }
         .padding(.horizontal, 30)
         .padding(.vertical, 0)
         .frame(height: 100)
+        .onChange(of: selectedIndex) {
+            if selectedIndex == 0 && !isEditing {
+                isSelecting = false
+                selectedThoughts.removeAll()
+            }
+        }
     }
+}
+
+struct Constants {
+    static let TextLightText: Color = Color(red: 0.52, green: 0.52, blue: 0.69)
 }
